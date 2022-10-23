efec9b786e3cf0247658e8dcbe524687b78d2863
Anwar Sh <alvaro.sh03@gmail.com>
2022-10-23 19:39:10 +0500

    Implement adding new anecdotes

--- redux-anecdotes/src/App.js
+++ redux-anecdotes/src/App.js
@@ -1,5 +1,5 @@
 import { useSelector, useDispatch } from 'react-redux'
-import { voteAnecdote } from './reducers/anecdoteReducer'
+import { voteAnecdote, createAnecdote } from './reducers/anecdoteReducer'
 
 const App = () => {
   const anecdotes = useSelector(state => state)
@@ -8,6 +8,15 @@ const App = () => {
   const vote = (id) => {
     console.log('vote', id)
     dispatch(voteAnecdote(id))
+  }
+
+  const addAnecdote = event => {
+    event.preventDefault()
+    const content = event.target.anecdote.value
+    console.log(`add anecdote '${content}'`)
+
+    dispatch(createAnecdote(content))
+    event.target.anecdote.value = ''
   }
 
   return (
@@ -25,9 +34,9 @@ const App = () => {
         </div>
       )}
       <h2>create new</h2>
-      <form>
-        <div><input /></div>
-        <button>create</button>
+      <form onSubmit={addAnecdote}>
+        <div><input name='anecdote'/></div>
+        <button type='submit'>create</button>
       </form>
     </div>
   )
