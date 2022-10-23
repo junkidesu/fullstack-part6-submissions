5b0a7656e36b5549b363f13dacc042a7d400267b
Anwar Sh <alvaro.sh03@gmail.com>
2022-10-23 19:18:00 +0500

    Implement VOTE action and its creator

--- redux-anecdotes/src/reducers/anecdoteReducer.js
+++ redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -23,7 +23,25 @@ const reducer = (state = initialState, action) => {
   console.log('state now: ', state)
   console.log('action', action)
 
+  if (action.type === 'VOTE') {
+    const id = action.data.id
+    const anecdoteToChange = state.find(a => a.id === id)
+
+    const changedAnecdote = {
+      ...anecdoteToChange,
+      votes: anecdoteToChange.votes + 1
+    }
+
+    return state.map(a => a.id !== id ? a : changedAnecdote)
+  }
   return state
 }
+
+export const voteAnecdote = id => ({
+  type: 'VOTE',
+  data: {
+    id
+  }
+})
 
 export default reducer
