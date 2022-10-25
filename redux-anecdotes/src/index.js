ce733094973f18c0067ff2c5b8866fa87d9afcc3
Anwar Sh <alvaro.sh03@gmail.com>
2022-10-25 13:00:08 +0500

    Add filter reducer to store

--- redux-anecdotes/src/index.js
+++ redux-anecdotes/src/index.js
@@ -5,11 +5,13 @@ import { Provider } from 'react-redux'
 import App from './App'
 import anecdoteReducer from './reducers/anecdoteReducer'
 import notificationReducer from './reducers/notificationReducer'
+import filterReducer from './reducers/filterReducer'
 
 const store = configureStore({
   reducer: {
     anecdotes: anecdoteReducer,
-    notification: notificationReducer
+    notification: notificationReducer,
+    filter: filterReducer
   }
 })
 
