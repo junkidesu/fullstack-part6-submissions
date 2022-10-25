745817130038533f860cdbeb521969143ef59236
Anwar Sh <alvaro.sh03@gmail.com>
2022-10-25 11:32:05 +0500

    Refactor UI to use ReduxToolkit

--- redux-anecdotes/src/index.js
+++ redux-anecdotes/src/index.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createStore } from 'redux'
+import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
 import App from './App'
-import reducer from './reducers/anecdoteReducer'
+import anecdoteReducer from './reducers/anecdoteReducer'
 
-const store = createStore(reducer)
+const store = configureStore({
+  reducer: {
+    anecdotes: anecdoteReducer
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
